cb3814c2daa6f7e7a5512f960e2cc944262f825c
rashedul-hridoy <iamrashedul4@gmail.com>
2022-12-06 19:43:32 +0600

    Full server updated from private repo

---
+++ photography-server/index.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.akesmbp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorize Access'});
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err){
+            return res.status(401).send({message: 'Unauthorize Access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+
+async function run(){
+    try{
+        const serviceCollection = client.db('photographydb').collection('services');
+        const reviewCollection = client.db('photographydb').collection('reviews');
+
+        //jwt api
+        app.post('/jwt', (req,res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+            res.send({token});
+        })
+
+        //service api
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        app.post('/services', async(req,res) =>{
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
+        //review api
+
+        app.get('/reviews', async (req,res) =>{
+            let query = {};
+            if(req.query.service){
+                query ={service : req.query.service}
+            }
+            const cursor = reviewCollection.find(query).sort( { time : -1 } );
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.get('/myreviews', verifyJWT, async(req,res) =>{
+            const decoded =req.decoded;
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'Unauthorize Access'});
+            }
+            let query = {};
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
+            const cursor = reviewCollection.find(query).sort( { time : -1 } );
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+
+        });
+        app.get('/myreviews/:id', verifyJWT,  async (req, res) => {
+
+            const decoded =req.decoded;
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'Unauthorize Access'});
+            }
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const review = await reviewCollection.findOne(query).sort( { time : -1 } );
+            res.send(review);
+        });
+
+        app.post('/reviews', async (req,res) =>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.msg
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    reviewMessage : status
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/myreviews/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Photography server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Photography server running on ${port}`);
+})
